ec997f9835c19a6fdb143ca605bccca87b5fe2a9
Bradley Bell <bradleyb@uw.edu>
2017-03-10 12:46:57 -0800

    update deprecated urlpatterns

--- panopto_tools/urls.py
+++ panopto_tools/urls.py
@@ -1,12 +1,13 @@
-from django.conf.urls import patterns, url
-from views.recorder_feed import RecorderFeed
+from django.conf.urls import url
 
-urlpatterns = patterns(
-    '',
+from views.recorder_feed import RecorderFeed
+from .views import recorderpreview
+
+urlpatterns = [
     url(r'^preview/(?P<recorder_id>[0-9a-f\-]+)(.jpe?g)?$',
-        'panopto_tools.views.recorderpreview.preview'),
+        recorderpreview.preview),
     url(r'^recordings/(?P<recorder_id>[0-9a-f\-]+)\.ics$', RecorderFeed()),
     url(r'^recordings/'
         r'(?P<building>[a-zA-Z0-9]+)[ +_-](?P<room>[a-zA-Z0-9]+)\.ics$',
         RecorderFeed()),
-)
+]
