c9d1bef1116f3288dbdc3dab578763506d480a6a
Bradley Bell <bradley.bell@gmail.com>
2022-01-26 20:30:50 -0800

    add reqs

--- setup.py
+++ setup.py
@@ -4,4 +4,13 @@ setup(
     name="django-panopto-tools",
     version="0.1",
     packages=find_packages(),
+    install_requires=[
+        'AuthZ-Group',
+        'Django',
+        'django-ical',
+        'Django-UserService',
+        'Pillow',
+        'UW-Panopto-Client',
+        'UW-RestClients-R25'
+    ]
 )
